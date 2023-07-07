9800a590085923f7ec3ac5aad5834cd2450fcbdc
Mario Antonioletti <mario@epcc.ed.ac.uk>
2023-07-07 09:28:59 +0100

    Make the unzipping quiet.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Unzip the data.
-unzip /input_data/JobsAcUk.zip -d /output_data/data/
+unzip -q /input_data/JobsAcUk.zip -d /output_data/data/
 
 # Copy the resource metadata
 cp ./resources.json /output_data/metadata/
